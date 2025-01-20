3b932e4c558f97ba82c2f9480c8f24d52dd4bbfb
Tania Moura <moura4design@gmail.com>
2025-01-20 17:49:32 +0100

    Logout button and inputs with add book

--- my-app/src/Components/BookList.js
+++ my-app/src/Components/BookList.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const BookList = () => {
+  const navigate = useNavigate();
+  const [bookTitle, setBookTitle] = useState('');
+
+  const handleLogout = () => {
+    navigate('/');
+  }
+
   return(
-    <>Book List</>
+    <React.Fragment>
+      <button className='btn btn-secondary mt-5 mb-5' onClick={handleLogout}>Logout</button>
+      <h3>List of the books</h3>
+      <div className="d-flex justify-content-center mb-4">
+        <input className="me-2" placeholder="Add title of the book" value={bookTitle} onChange={(e) => {setBookTitle(e.target.value)}} />
+        <input className="me-2" placeholder="Add author of the book" />
+        <button className='btn btn-info'>Add new book</button>
+      </div>
+    </React.Fragment>
   )
 }
 
