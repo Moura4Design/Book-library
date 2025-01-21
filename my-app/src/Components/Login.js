dc594fd9a12c066c5ce062c8fa951863c6d2913b
Tania Moura <moura4design@gmail.com>
2025-01-21 12:10:53 +0100

    update the login

--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const handleLogin = () => {
-    navigate('/booklist');
+    console.log('email ==>', email)
+    if(email === 'Tania') {
+      navigate('/booklist');
+      setErrorMessage(false)
+    } else {
+      setErrorMessage(true)
+    }
+
   }
 
+  console.log('email 1 ==>', email)
   return(
     <React.Fragment>
       <h3 className="mt-5 mb-5">Login page</h3>
+      {errorMessage ? <p className="text-danger">Login Failed.</p> : null}
       <div className="d-flex justify-content-center mb-3">
         <label className="col-sm-2 col-form-label">Email:</label>
         <div className="col-sm-4">
-          <input className="form-control" />
+          <input className="form-control" value={email} onChange={(e) => {setEmail(e.target.value)}} />
         </div>
       </div>
       <div className="d-flex justify-content-center mb-3">
